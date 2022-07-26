4d7bf923f38b9ad3eb5793e7fff998b411e9f0d7
Daniel Azocar <danielazocardev@Daniels-MacBook-Air.local>
2022-07-25 20:47:13 -0400

    fix pdf file

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -25,7 +25,7 @@ export const Hero = () => {
           <Tooltip title="download my Resume" arrow>
             <IconButton
               color="primary"
-              href="src/assets/DanielAzocarResume.pdf"
+              href="/assets/DanielAzocarResume.pdf"
               download
             >
               <DownloadForOfflineIcon fontSize="large" />
